c68dfe2f99f31ca8bde37bbf67dc16ad6e90e715
Egoitz <egoitttzzz@gmail.com>
2026-02-05 12:43:49 +0100

    Se han añadido dos metodos mas el de multiplicar y dividir

--- src/calculadora22/Calculadora22.java
+++ src/calculadora22/Calculadora22.java
@@ -8,5 +8,13 @@ public class Calculadora22 {
 	public int restar(int a, int b) { 
 		return a - b;
 	}
+	public int multiplicar(int a, int b) { 
+		return a * b;
+	}
+
+	public double dividir(int a, int b) { 
+		return (double) a / b;
+	}
+
 
 }
